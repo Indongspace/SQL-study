9208935b71fec2b416a8cc6ca78a27759653996a
SongInDong <indongspace98@gmail.com>
2025-01-02 23:23:24 +0900

    MYSQL coding test

--- 프로그래머스코딩테스트_MySQL/LV.4 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기 repeat.sql
+++ 프로그래머스코딩테스트_MySQL/LV.4 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기 repeat.sql
@@ -1,13 +1,32 @@
+WITH nov_able AS (
+    SELECT
+        *
+    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY 
+    WHERE
+        end_date >= '2022-11-01' AND start_date <= '2022-11-30'
+), base AS (
+    SELECT
+        c.car_id,
+        c.car_type,
+        c.daily_fee,
+        n.start_date,
+        n.end_date
+    FROM CAR_RENTAL_COMPANY_CAR AS c
+    LEFT JOIN nov_able AS n
+    ON c.car_id = n.car_id
+    WHERE
+        car_type = '세단' OR car_type = 'SUV'
+)
 SELECT
-    c.car_id,
-    c.car_type,
-    c.daily_fee,
-    h.start_date,
-    h.end_date
-FROM CAR_RENTAL_COMPANY_CAR AS c
-LEFT JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY AS h
-ON c.car_id = h.car_id
+    b.car_id,
+    b.car_type,
+    ROUND(b.daily_fee * 30 * (1 - (d.discount_rate / 100))) AS fee
+FROM base AS b
+INNER JOIN (SELECT * FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN WHERE duration_type = '30일 이상') AS d
+ON b.car_type = d.car_type
 WHERE
-    c.car_type = '세단' OR c.car_type = 'SUV' AND
-    h.start_date <= '2022-11-30' AND h.end_date >= '2022-11-01'
-    
+    start_date IS NULL AND
+    ROUND(b.daily_fee * 30 * (1 - (d.discount_rate / 100))) >= 500000 AND
+    ROUND(b.daily_fee * 30 * (1 - (d.discount_rate / 100))) < 2000000
+ORDER BY
+    fee DESC, car_type ASC, car_id DESC
