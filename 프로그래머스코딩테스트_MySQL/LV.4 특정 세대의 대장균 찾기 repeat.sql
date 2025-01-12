08bfa31860cb8301ee339c2fc84d7189837a624c
SongInDong <indongspace98@gmail.com>
2025-01-12 23:26:13 +0900

    MYSQL coding test

---
+++ 프로그래머스코딩테스트_MySQL/LV.4 특정 세대의 대장균 찾기 repeat.sql
@@ -0,0 +1,8 @@
+SELECT
+    id
+FROM ECOLI_DATA
+WHERE
+    parent_id IN (SELECT id FROM ECOLI_DATA WHERE parent_id IN (SELECT id FROM ECOLI_DATA WHERE parent_id IS NULL))
+ORDER BY
+    id ASC
+    
