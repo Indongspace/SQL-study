a8d0ef155e04fdc240b23ebbea3b25e1988d49ac
SongInDong <indongspace98@gmail.com>
2025-02-03 00:30:47 +0900

    SQLite coding test

--- solvesql코딩테스트_SQLite/LV.3 게임 평점 예측하기 1.sql
+++ solvesql코딩테스트_SQLite/LV.3 게임 평점 예측하기 1.sql
@@ -10,32 +10,25 @@ WITH null_data AS (
   FROM games
   WHERE
     year >= 2015 AND
-    critic_score IS NULL OR critic_count IS NULL OR user_score IS NULL OR user_count IS NULL 
-), base AS (
-  SELECT
-    game_id,
-    name,
-    genre_id,
-    critic_score,
-    critic_count,
-    user_score,
-    user_count
-  FROM games
-  WHERE
-    year >= 2015 AND critic_score IS NOT NULL AND critic_count IS NOT NULL AND user_score IS NOT NULL AND user_count IS NOT NULL
+    (critic_score IS NULL OR critic_count IS NULL OR user_score IS NULL OR user_count IS NULL)
 ), avg_score AS (
   SELECT
     genre_id,
-    AVG(critic_score) AS avg_critic_score,
-    CEIL(AVG(critic_count)) AS avg_critic_count,
-    AVG(user_score) AS avg_user_score,
-    CEIL(AVG(user_count)) AS avg_user_count 
-  FROM base
+    ROUND(AVG(critic_score), 3) AS avg_critic_score,
+    CAST(AVG(critic_count) AS INTEGER) + (CASE WHEN AVG(critic_count) > CAST(AVG(critic_count) AS INTEGER) THEN 1 ELSE 0 END) AS avg_critic_count,
+    ROUND(AVG(user_score), 3) AS avg_user_score,
+    CAST(AVG(user_count) AS INTEGER) + (CASE WHEN AVG(user_count) > CAST(AVG(user_count) AS INTEGER) THEN 1 ELSE 0 END) AS avg_user_count  
+  FROM games
   GROUP BY
     genre_id
 )
 SELECT
-  game_id,
-  name,
-  
-FROM null_data
+  n.game_id,
+  n.name,
+  COALESCE(n.critic_score, a.avg_critic_score) AS critic_score,
+  COALESCE(n.critic_count, a.avg_critic_count) AS critic_count,
+  COALESCE(n.user_score, a.avg_user_score) AS user_score,
+  COALESCE(n.user_count, a.avg_user_count) AS user_count
+FROM null_data AS n 
+LEFT JOIN avg_score AS a 
+ON n.genre_id = a.genre_id 
