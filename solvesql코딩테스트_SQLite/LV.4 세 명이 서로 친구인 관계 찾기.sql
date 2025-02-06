0be79c12d25c9e87e905d512b825933135890831
SongInDong <indongspace98@gmail.com>
2025-02-07 02:08:30 +0900

    SQLite coding test

--- solvesql코딩테스트_SQLite/LV.4 세 명이 서로 친구인 관계 찾기.sql
+++ solvesql코딩테스트_SQLite/LV.4 세 명이 서로 친구인 관계 찾기.sql
@@ -1,4 +1,14 @@
 SELECT
-  *
-FROM edges
-LIMIT 4000
+  e1.user_a_id,
+  e1.user_b_id,
+  e2.user_b_id AS user_c_id
+FROM edges AS e1
+INNER JOIN edges AS e2
+ON e1.user_b_id = e2.user_a_id
+INNER JOIN edges AS e3
+ON e1.user_a_id = e3.user_a_id AND e2.user_b_id = e3.user_b_id
+WHERE
+  3820 IN (e1.user_a_id, e1.user_b_id, e2.user_b_id) AND
+  e1.user_a_id < e1.user_b_id AND
+  e1.user_b_id < e2.user_b_id
+  
