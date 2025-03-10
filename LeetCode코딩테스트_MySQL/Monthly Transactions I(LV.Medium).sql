6aa74f052af44287fd5d833aae23122802c7e30e
SongInDong <indongspace98@gmail.com>
2025-03-10 22:14:14 +0900

    MYSQL coding test

---
+++ LeetCode코딩테스트_MySQL/Monthly Transactions I(LV.Medium).sql
@@ -0,0 +1,10 @@
+SELECT
+    DATE_FORMAT(trans_date, '%Y-%m') AS month,
+    country,
+    COUNT(*) AS trans_count,
+    COUNT(CASE WHEN state = 'approved' THEN 1 END) AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM transactions
+GROUP BY
+    DATE_FORMAT(trans_date, '%Y-%m'), country
