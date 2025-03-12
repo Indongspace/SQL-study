5f35c3416e596386ab10292b94278e56138a1ecc
SongInDong <indongspace98@gmail.com>
2025-03-12 23:43:03 +0900

    MYSQL coding test

---
+++ LeetCode코딩테스트_MySQL/Product Price at a Given Date(LV.Medium).sql
@@ -0,0 +1,25 @@
+WITH change_under AS (
+    SELECT
+        product_id,
+        MAX(change_date) AS change_date
+    FROM products
+    WHERE
+        change_date <= '2019-08-16'
+    GROUP BY
+        product_id
+)
+SELECT
+    product_id,
+    10 AS price
+FROM products
+GROUP BY
+    product_id
+HAVING
+    MIN(change_date) > '2019-08-16'
+UNION
+SELECT
+    product_id,
+    new_price AS price
+FROM products
+WHERE
+    (product_id, change_date) IN (SELECT product_id, change_date FROM change_under)
