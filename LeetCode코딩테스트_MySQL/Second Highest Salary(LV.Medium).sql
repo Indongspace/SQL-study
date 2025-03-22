2c6039e051aff7a6bd7208dd64b4c24d141a529b
SongInDong <indongspace98@gmail.com>
2025-03-22 23:55:32 +0900

    MYSQL coding test

--- LeetCode코딩테스트_MySQL/Second Highest Salary(LV.Medium).sql
+++ LeetCode코딩테스트_MySQL/Second Highest Salary(LV.Medium).sql
@@ -1,9 +1,12 @@
 # 쿼리를 작성하는 목표, 확인할 지표 : 두번째로 높은 연봉 출력 / salary
-# 쿼리 계산 방법 : 1. salary 내림차순 -> 2. LIMIT 1 OFFSET -> 3. 1과 2의 결과를 CTE로 -> 4. CASE WHEN 으로 일치하는 수 가져오기 없으면 NULL 
+# 쿼리 계산 방법 : 1. 조건문으로 max보다 작은 경우만 -> 2. 조건에 맞는 값 중에 max값(=2번째로 큰 값) -> 3. coalesce로 없으면 null
 # 데이터의 기간 : x
 # 사용할 테이블 : employee
 # JOIN KEY : x
 # 데이터 특징 : x
 SELECT
-    salary
+    DISTINCT COALESCE(MAX(salary), NULL) AS secondhighestsalary
 FROM employee
+WHERE
+    salary < (SELECT MAX(salary) FROM employee)
+    
